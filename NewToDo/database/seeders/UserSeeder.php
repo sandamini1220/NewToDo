1d9b5c1e1c54f5db85959366acea61f32e843d6a
sandamini1220 <161709902+sandamini1220@users.noreply.github.com>
2025-07-12 18:34:52 +0530

    correction

--- NewToDo/database/seeders/UserSeeder.php
+++ NewToDo/database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
             'name' => 'Admin',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
-            'role' => 'admin',  // make sure your users table has 'role' column (or 'usertype' if you named it that)
+            'role' => 'admin',  
         ]);
 
         User::create([
